6161f4ca30fb5e04aa32ec78f476a010485113fc
afnanshihab <afnan.shihab@yahoo.com>
2021-06-06 16:45:40 +0300

    creating new html , css and java scripts files

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let userName = prompt('Please enter your name!');
+alert('Welcome ' + userName + ' to our website');
+
+console.log(`You name is ${userName}, welcome to our website`);
+
+let userAge = prompt('Please enter your Age!');
+alert('your age is' + userAge );
+
+console.log(`your Age is ${userAge}, we wish a whole life for you`);
+
+let userWeight = prompt('Please enter your weight');
+alert('your weight is' + userWeight + ' , Good luck for having a perfect weight');
+
+console.log(`Your weight is ${userWeight} `);
+
+let userHeight = prompt('How much your height!');
+alert('your height is  ' + userHeight );
+
+console.log(`your height is  ${userHeight} `);
+
+
+
